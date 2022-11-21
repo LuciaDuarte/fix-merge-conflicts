03fb7162dc8944016967e09d73a3199476773d42
Lucia Duarte <analuciacfduarte@gmail.com>
2022-11-21 22:01:45 +0000

    Add base project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/users/:user/books/:bookId', (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
+});
+
+app.get('/search', (req, res) => {
+  res.send(req.query);
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  res.send('Login done');
+});
+
+app.listen('3000', () => console.log('App is running!'));
